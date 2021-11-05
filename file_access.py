8340120ea0af39f753f86d9ca8ab7ba5d37a0886
Maurizio Tomasi <ziotom78@gmail.com>
2021-11-05 11:21:23 +0100

    Implement a local cache for HDF5 files
    This speeds up a lot the loading of files from the Web database.

--- file_access.py
+++ file_access.py
@@ -17,15 +17,31 @@ dealing with.
 """
 
 from collections import namedtuple
+from pathlib import Path
 import os
-from shutil import copyfileobj
+from shutil import copyfileobj, copyfile
 from tempfile import NamedTemporaryFile
 import urllib.parse as urlparse
 import urllib.request as urlreq
+import logging as log
 
 import h5py
 import numpy as np
 import simplejson as json
+
+# This is the place where HDF5 files downloaded from the Web are saved
+CACHE_PATH = Path.home() / ".cache" / "striptun"
+# Ensure that the cache directory exist
+CACHE_PATH.mkdir(parents=True, exist_ok=True)
+
+
+def default_hdf5_file_name(metadata):
+    "Build a HDF5 file name out of test metadata"
+
+    return (
+        CACHE_PATH
+        / f"STRIP{metadata['polarimeter_number']:02d}_test{metadata['id']:04d}.h5"
+    )
 
 
 def download_json_from_url(url):
@@ -184,7 +200,7 @@ def load_timestream(file_path):
     Return a pair consisting of a dictionary containing the medatada and a
     Timestream object."""
 
-    if not urlparse.splittype(file_path)[0]:
+    if isinstance(file_path, Path) or (not urlparse.splittype(file_path)[0]):
         # Local path
         ext = os.path.splitext(file_path)[1]
         if ext.lower() == ".txt":
@@ -193,9 +209,13 @@ def load_timestream(file_path):
             return load_hdf5_file(file_path)
     else:
         # URL
-        req = urlreq.urlopen(file_path)
+        url = file_path
+        req = urlreq.urlopen(url)
         content_type = req.info().get_content_type()
 
+        save_to_cache = False
+        result = None
+        default_hdf5_file_name
         # We are *forced* to create a named temporary file and close it
         # before reading, because h5py does not support reading from
         # file-like objects like BytesIO or an already opened TemporaryFile
@@ -203,12 +223,24 @@ def load_timestream(file_path):
             h5_file_name = h5_file.name
             if content_type == "application/json":
                 metadata = json.loads(req.read().decode("utf-8"))
-                download_test(file_path, metadata, h5_file)
+                file_path = default_hdf5_file_name(metadata)
+                if file_path.is_file():
+                    log.info(f'Test {url} found in cache "{file_path}"')
+                    _, result = load_timestream(file_path)
+                else:
+                    save_to_cache = True
+                    download_test(url, metadata, h5_file)
             elif content_type == "application/hdf5":
                 copyfileobj(req, h5_file)
             else:
                 raise ValueError('unknown content type: "{0}"'.format(content_type))
 
-        result = load_hdf5_file(h5_file_name)[1]
+        if result is None:
+            result = load_hdf5_file(h5_file_name)[1]
+
+        if save_to_cache:
+            log.info(f'Copy file downloaded from {url} to cache "{file_path}"')
+            copyfile(src=h5_file_name, dst=file_path)
+
         os.remove(h5_file_name)
         return metadata, result
